f24c5620eabe7e58a804ae61aa9cacbd9623b0a5
ERLMKea <erlm@kea.dk>
2023-01-31 13:01:38 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    private static void out(String s1) {
+        System.out.println(s1);
+    }
+
+    private static void out(int i1) {
+        System.out.println(i1);
+    }
+
+    private static void out(Integer i2) {
+        System.out.println("Jeg er Integer:" + i2);
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+}
